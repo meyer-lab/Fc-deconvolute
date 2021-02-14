3990d04a9fa0ce962bac9c616d395933d0e2844a
Amanda <ajtsao1@gmail.com>
2021-02-13 18:05:07 -0800

    updated nnls notebook and fixed path

--- deconv/figures/common.py
+++ deconv/figures/common.py
@@ -4,8 +4,8 @@ import scipy as sp
 from scipy.optimize import nnls
 
 def load_tables():
-    antiD = pd.read_csv("./deconv/data/anti-D.csv")
-    antiT = pd.read_csv("./deconv/data/anti-TNP.csv")
+    antiD = pd.read_csv("../data/anti-D.csv")
+    antiT = pd.read_csv("../data/anti-TNP.csv")
     glycan_list = list(antiD.columns.values[7:])
     A_antiD = antiD.iloc[:, 7:].values
     A_antiTNP = antiT.iloc[:, 7:].values
@@ -13,10 +13,10 @@ def load_tables():
 
 def load_figures():
     # binding in doners with specific r3a gamma receptor
-    figA = pd.read_csv("./deconv/data/fig3a.csv")
+    figA = pd.read_csv("../data/fig3a.csv")
     # combination of 4 independent receptors?
     # number of the mixture (4 data points for each mixture), cytotoxicity percentage 
-    figB = pd.read_csv("./deconv/data/fig3b.csv")
+    figB = pd.read_csv("../data/fig3b.csv")
     adcc_3a = figA.iloc[:, 0]
     adcc_3b = figB.iloc[:, 0]
     return (adcc_3a, adcc_3b)
