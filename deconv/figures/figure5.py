55312bc7c544012dabb65854c6ec21646a665838
Amanda <ajtsao1@gmail.com>
2021-03-08 02:35:30 -0800

    update binding plots

--- deconv/figures/figure5.py
+++ deconv/figures/figure5.py
@@ -3,7 +3,7 @@ from .common import subplotLabel, getSetup
 from ..imports import load_tables, infer_x, load_bindingData
 
 def makeFigure():
-    ax, f = getSetup((6, 3), (2, 4))
+    ax, f = getSetup((6, 12), (4, 2))
 
     fig2A, fig2B, fig2C, fig2D, fig2E, fig2F, fig2G, fig2H = load_bindingData()
 
@@ -30,16 +30,40 @@ def makeFigure():
 
     glycans_2A = infer_x(A_antiD, mean_2A)
     glycans_2B = infer_x(A_antiD, mean_2B)
+    glycans_2C = infer_x(A_antiD, mean_2C)
+    glycans_2D = infer_x(A_antiD, mean_2D)
+    glycans_2E = infer_x(A_antiD, mean_2E)
+    glycans_2F = infer_x(A_antiD, mean_2F)
+    glycans_2G = infer_x(A_antiD, mean_2G)
+    glycans_2H = infer_x(A_antiD, mean_2H)
 
     ax[0].bar(glycan_list, glycans_2A)
     ax[0].set_title("FcgRI Binding (Fig. 2A)")
-    ax[0].set_xlabel("Glycans")
-    ax[0].set_xticklabels(glycan_list, rotation=90)
 
     ax[1].bar(glycan_list, glycans_2B)
-    ax[1].set_title("FcgRIIa Binding (Fig. 2B)")
-    ax[1].set_xlabel("Glycans")
-    ax[1].set_xticklabels(glycan_list, rotation=90)
+    ax[1].set_title("FcgRIIa-131H Binding (Fig. 2B)")
+
+    ax[2].bar(glycan_list, glycans_2C)
+    ax[2].set_title("FcgRIIa-131R Binding (Fig. 2C)")
+
+    ax[3].bar(glycan_list, glycans_2D)
+    ax[3].set_title("FcgRIIb Binding (Fig. 2D)")
+
+    ax[4].bar(glycan_list, glycans_2E)
+    ax[4].set_title("FcgRIIIa-158F Binding (Fig. 2E)")
+
+    ax[5].bar(glycan_list, glycans_2F)
+    ax[5].set_title("FcgRIIIa-158V Binding (Fig. 2F)")
+
+    ax[6].bar(glycan_list, glycans_2G)
+    ax[6].set_title("FcgRIIIb-NA1 Binding (Fig. 2G)")
+
+    ax[7].bar(glycan_list, glycans_2H)
+    ax[7].set_title("FcgRIIIb-NA2 Binding (Fig. 2H)")
+
+    for x in range(8):
+        ax[x].set_xlabel("Glycans")
+        ax[x].set_xticklabels(glycan_list, rotation=90)
 
     # Add subplot labels
     subplotLabel(ax)
