12d707ff049c796b8ed77ec700f6bcb5fa74798a
sumedha-k <sumedha.kanthamneni@gmail.com>
2021-04-22 20:22:26 -0700

    removed double anti-D

--- deconv/figures/figure3.py
+++ deconv/figures/figure3.py
@@ -16,27 +16,27 @@ def makeFigure():
 
     double_3a = np.concatenate((mean_3a, mean_3a), axis=0)
     double_3b = np.concatenate((mean_3b, mean_3b), axis=0)
-    double_antiD = np.concatenate((A_antiD, A_antiD), axis=0)
+    #double_antiD = np.concatenate((A_antiD, A_antiD), axis=0)
 
     glycans_3a = infer_x(A, double_3a)
     glycans_3b = infer_x(A, double_3b)
 
-    original_3a = infer_x(double_antiD, double_3a)
-    original_3b = infer_x(double_antiD, double_3b)
+    #original_3a = infer_x(double_antiD, double_3a)
+    #original_3b = infer_x(double_antiD, double_3b)
 
-    width = 0.35
+    #width = 0.35
     ind = np.arange(len(glycan_list))
 
-    ax[0].bar(ind - width/2, glycans_3a, width, label = 'Anti-D and Anti-TNP')
-    ax[0].bar(ind + width/2, original_3a, width, label = 'Double Anti-D')
+    ax[0].bar(ind, glycans_3a, label = 'Anti-D and Anti-TNP')
+    #ax[0].bar(ind + width/2, original_3a, width, label = 'Double Anti-D')
     ax[0].set_title("Anti-D and Anti-TNP (Fig. 3A)")
     ax[0].set_xlabel("Glycans")
     ax[0].set_xticklabels(glycan_list, rotation=90)
     ax[0].set_xticks(ind)
     ax[0].legend()
 
-    ax[1].bar(ind - width/2, glycans_3b, width, label = 'Anti-D and Anti-TNP')
-    ax[1].bar(ind + width/2, original_3b, width, label = 'Double Anti-D')
+    ax[1].bar(ind, glycans_3b, label = 'Anti-D and Anti-TNP')
+    #ax[1].bar(ind + width/2, original_3b, width, label = 'Double Anti-D')
     ax[1].set_title("Anti-D and Anti-TNP (Fig. 3B)")
     ax[1].set_xlabel("Glycans")
     ax[1].set_xticklabels(glycan_list, rotation=90)
