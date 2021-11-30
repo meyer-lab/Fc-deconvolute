1910e2a83f7cd4e8cb87f3e7178522afdde20561
Eva Hunter <evahunter@g.ucla.edu>
2021-11-29 18:18:02 -0800

    new fig 3 w error

--- deconv/figures/figure3.py
+++ deconv/figures/figure3.py
@@ -6,19 +6,24 @@ import numpy as np
 from ..imports import load_dekkers
 
 def makeFigure():
+    #imports and formats
     ax, f = getSetup((12, 6), (1, 2))
     trace = getEmceeTrace()
     data_dekkers = load_dekkers()
+    pca = PCA()
+    pca2 = PCA()
 
     activity_scores = (trace.posterior.activity_scores[0])
     activity_loadings = (trace.posterior.activity_loadings[0])
     median_scores = np.median(activity_scores, axis = 0)
     median_loadings = np.median(activity_loadings, axis = 0)
-    
-    pca = PCA()
-    pca2 = PCA()
-    scores_pca = pca.fit_transform(median_scores)
-    loadings_pca = pca2.fit_transform(median_loadings)
+
+    scorespca = pca.fit(median_scores)
+    loadingspca =pca2.fit(median_loadings)
+    pc1_var = pca.explained_variance_ratio_[0]
+    pc2_var = pca.explained_variance_ratio_[1]
+    p1var = pca2.explained_variance_ratio_[0]
+    p2var = pca2.explained_variance_ratio_[1]
 
     #set up matrix w errors: SCORES
     p33_s = np.zeros((24,3))
@@ -27,9 +32,6 @@ def makeFigure():
         p33_s[:, i] = np.percentile(median_scores[:,i], 33)
         p66_s[:,i] =np.percentile(median_scores[:,i], 66)
 
-    err_pca_33s = pca.transform(p33_s)
-    err_pca_66s = pca.transform(p66_s)
-
     #set up matrix w errors: LOADINGS
     p33_l = np.zeros((3,12))
     p66_l = np.zeros((3,12))
@@ -37,30 +39,24 @@ def makeFigure():
         p33_l[:, i] = np.percentile(median_loadings[:,i], 33)
         p66_l[:,i] =np.percentile(median_loadings[:,i], 66)
 
-    err_pca_33l = pca2.transform(p33_l)
-    err_pca_66l = pca2.transform(p66_l)
-
-
     ax, f = getSetup((9, 4), (1, 2))
 
-    #ax[0].scatter(scores_pca[:, 0], scores_pca[:, 1])
     ax[0].set_title("Activity Scores")
-    ax[0].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=pca.explained_variance_ratio_[0]))
-    ax[0].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=pca.explained_variance_ratio_[1]))
-    ax[0].errorbar(scores_pca[:, 0], scores_pca[:, 1], yerr=[err_pca_33s[:,1], err_pca_66s[:,1]], xerr = [err_pca_33s[:,0], err_pca_66s[:,0]], fmt='o')
+    ax[0].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=pc1_var))
+    ax[0].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=pc2_var))
+    ax[0].errorbar(median_scores[:, 0], median_scores[:, 1], yerr=[p33_s[:,1], p66_s[:,1]], xerr = [p33_s[:,0], p66_s[:,0]], fmt='o')
     glycans = data_dekkers["glycans"]
 
-    scores = pd.DataFrame(scores_pca)
+    scores = pd.DataFrame(median_scores)
     for i in range(24):
         ax[0].annotate(glycans[i], (scores.iloc[i, 0], scores.iloc[i, 1]))
 
-    #ax[1].scatter(loadings_pca[:, 0], loadings_pca[:, 1])
     ax[1].set_title("Activity Loadings")
-    ax[1].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[0]))
-    ax[1].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[1]))
-    ax[1].errorbar(loadings_pca[:, 0], loadings_pca[:, 1], yerr=[err_pca_33l[:,1], err_pca_66l[:,1]], xerr = [err_pca_33l[:,0], err_pca_66l[:,0]], fmt='o')
+    ax[1].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=p1var))
+    ax[1].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=p2var))
+    ax[1].errorbar(median_loadings[:, 0], median_loadings[:, 1], yerr=[p33_l[:,1], p66_l[:,1]], xerr = [p33_l[:,0], p66_l[:,0]], fmt='o')
     labels = ["FcRI", 'FcRII', 'FcRIII']
-    loadings = pd.DataFrame(loadings_pca)
+    loadings = pd.DataFrame(median_loadings)
 
     for i in range(3):
         ax[1].annotate(labels[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
