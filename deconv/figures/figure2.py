246ee6e18838d4d5b2730eafbd45174facedfcb2
Aaron Meyer <ameyer@ucla.edu>
2021-12-07 14:46:12 -0800

    Fixed

--- deconv/figures/figure2.py
+++ deconv/figures/figure2.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from .common import getSetup
 from ..emceeDeconv import getEmceeTrace
 from ..imports import load_dekkers
@@ -11,27 +10,23 @@ def makeFigure():
     glycans = data_dekkers["glycans"]
     l = ['Binding FcγRIa', 'Binding FcγRIIa 131H', 'Binding FcγRIIa 131R', 'Binding FcγRIIb/c', 'Binding FcγRIIIa 158F', 'Binding FcγRIIIa 158V', 'Binding Fc-FcγRIIIb NA1', 'Binding Fc-FcγRIIIb NA2', 'ADCC FcγRIIIA158F/F', 'ADCC FcγRIIIA158V/V', 'Complement Activation C1q', 'Complement Activation C4']
 
-    
     trace = getEmceeTrace()
 
     activity_scores = trace.posterior.activity_scores[0]
     activity_loadings = trace.posterior.activity_loadings[0]
 
-    median_scores = np.median(activity_scores, axis=0) 
-    p33_scores = np.percentile(activity_scores, 33, axis=0)
-    p66_scores = np.percentile(activity_scores, 66, axis=0)
+    full = np.einsum("ijk,ikl->ijl", activity_scores, activity_loadings)
 
-    median_loadings = np.median(activity_loadings, axis=0)
-    p33_loadings = np.percentile(activity_loadings, 33, axis=0)
-    p66_loadings = np.percentile(activity_loadings, 66, axis=0)
+    qqs = np.quantile(full, (0.33, 0.5, 0.66), axis=0)
+    median = qqs[1, :, :]
+    p33 = median - qqs[0, :, :]
+    p66 = qqs[2, :, :] - median
 
-    measurement_glycan_matrix = np.transpose(np.dot(median_scores, median_loadings))
-    p33 = np.transpose(np.dot(p33_scores, p33_loadings))
-    p66 = np.transpose(np.dot(p66_scores, p66_loadings))
+    print(median.shape)
+    print(len(glycans))
 
-
-    for i in range(len(measurement_glycan_matrix)):
-        ax[i].errorbar(glycans, measurement_glycan_matrix[i], yerr=[p33[i], p66[i]], fmt='o')
+    for i in range(median.shape[1]):
+        ax[i].errorbar(glycans, median[:, i], yerr=[p33[:, i], p66[:, i]], fmt='o')
         ax[i].set_title(l[i])
         ax[i].set_xticklabels(glycans, rotation=90)
 
