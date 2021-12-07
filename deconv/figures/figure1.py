0f39e5c91c52b14a43c8e774f9ccf94141c661fa
Sumedha Kanthamneni <sumedha.kanthamneni@gmail.com>
2021-12-07 14:26:32 -0800

    added complement activation to PCA (#32)
    * added complement activation to PCA
    
    * label update

--- deconv/figures/figure1.py
+++ deconv/figures/figure1.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def makeFigure():
-    ax, f = getSetup((9, 3), (1, 3))
+    ax, f = getSetup((20, 6), (1, 3))
 
     data_dekkers = load_dekkers()
 
@@ -15,7 +15,11 @@ def makeFigure():
 
     mean_3a = data_dekkers["meanADCC3a"]
     mean_3b = data_dekkers["meanADCC3b"]
-    data = [mean_3a, mean_3b] + mean_binding
+
+    mean_4a = data_dekkers["meanCompAct4a"]
+    mean_4b = data_dekkers["meanCompAct4b"]
+
+    data = [mean_3a, mean_3b, mean_4a, mean_4b] + mean_binding
 
     # TODO: add compement activation
 
@@ -28,14 +32,18 @@ def makeFigure():
     ax[0].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[0]))
     ax[0].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[1]))
 
-    l = ['adcc3a', 'adcc3b', 'bindingA', 'bindingB', 'bindingC', 'bindingD', 'bindingE', 'bindingF', 'bindingG', 'bindingH']
+    mixtures = data_dekkers["mixtures"]
+    for i in range(20):
+        ax[0].annotate(mixtures[i], (data_new[i,0], data_new[i,1]))
+
+    l = ['ADCC FcγRIIIA158F/F', 'ADCC FcγRIIIA158V/V', 'Complement Activation C1q', 'Complement Activation C4', 'Binding FcγRIa', 'Binding FcγRIIa 131H', 'Binding FcγRIIa 131R', 'Binding FcγRIIb/c', 'Binding FcγRIIIa 158F', 'Binding FcγRIIIa 158V', 'Binding Fc-FcγRIIIb NA1', 'Binding Fc-FcγRIIIb NA2']
     loadings = pd.DataFrame(pca2.components_.T[:, :2], columns=['PC1', 'PC2'], index=l)
     ax[1].scatter(loadings.iloc[:, 0], loadings.iloc[:, 1])
     ax[1].set_title("Loadings")
     ax[1].set_xlabel("Component 1 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[0]))
     ax[1].set_ylabel("Component 2 ({ratio:.2f})".format(ratio=pca2.explained_variance_ratio_[1]))
 
-    for i in range(10):
+    for i in range(12):
         ax[1].annotate(l[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
 
     acc_variance = pca2.explained_variance_ratio_.copy()
