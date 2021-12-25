a55c649bb0ba17a79101866ab34392d51988511e
Eva Hunter <75399938+evahunter@users.noreply.github.com>
2021-12-24 16:17:57 -0800

    update fig 3 (#37)

--- deconv/figures/figure3.py
+++ deconv/figures/figure3.py
@@ -7,7 +7,6 @@ from ..imports import load_dekkers
 
 def makeFigure():
     #imports and formats
-    ax, f = getSetup((12, 6), (1, 2))
     trace = getEmceeTrace()
     data_dekkers = load_dekkers()
 
@@ -28,11 +27,13 @@ def makeFigure():
     lowErrL = np.subtract(median_loadings, ac33)
     highErrL = np.subtract(ac66, median_loadings)
 
-    ax, f = getSetup((9, 4), (1, 2))
+    ax, f = getSetup((7, 7), (2, 2))
 
     ax[0].set_title("Activity Scores")
     ax[0].set_xlabel("Component 1")
     ax[0].set_ylabel("Component 2")
+    ax[0].set_ylim([0,1.5])
+    ax[0].set_xlim([0,7])
     ax[0].errorbar(median_scores[:, 0], median_scores[:, 1], yerr=[lowErrS[:, 1], highErrS[:, 1]], xerr=[lowErrS[:, 0], highErrS[:, 0]], fmt='o')
     glycans = data_dekkers["glycans"]
 
@@ -43,6 +44,8 @@ def makeFigure():
     ax[1].set_title("Activity Loadings")
     ax[1].set_xlabel("Component 1")
     ax[1].set_ylabel("Component 2")
+    ax[1].set_ylim([0,1])
+    ax[1].set_xlim([0,1])
     ax[1].errorbar(median_loadings[:, 0], median_loadings[:, 1], yerr=[lowErrL[:, 1], highErrL[:, 1]], xerr=[lowErrL[:, 0], highErrL[:, 0]], fmt='o')
     labels = [
         'ADCC FcγRIIIA158F/F',
@@ -61,6 +64,28 @@ def makeFigure():
 
     for i in range(12):
         ax[1].annotate(labels[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
+
+    ax[2].set_title("Activity Scores")
+    ax[2].set_xlabel("Component 1")
+    ax[2].set_ylabel("Component 3")
+    ax[2].set_ylim([0,5])
+    ax[2].set_xlim([0,7])
+    ax[2].errorbar(median_scores[:, 0], median_scores[:, 2], yerr=[lowErrS[:, 2], highErrS[:, 2]], xerr=[lowErrS[:, 0], highErrS[:, 0]], fmt='o')
+    
+
+    scores = pd.DataFrame(median_scores)
+    for i in range(24):
+        ax[2].annotate(glycans[i], (scores.iloc[i, 0], scores.iloc[i, 2]))
+
+    ax[3].set_title("Activity Loadings")
+    ax[3].set_xlabel("Component 1")
+    ax[3].set_ylabel("Component 3")
+    ax[3].set_ylim([0,0.3])
+    ax[3].set_xlim([0,1])
+    ax[3].errorbar(median_loadings[:, 0], median_loadings[:, 2], yerr=[lowErrL[:, 2], highErrL[:, 2]], xerr=[lowErrL[:, 0], highErrL[:, 0]], fmt='o')
+
+    for i in range(12):
+        ax[3].annotate(labels[i], (loadings.iloc[i, 0], loadings.iloc[i, 2]))
 
     # Add subplot labels
     subplotLabel(ax)
