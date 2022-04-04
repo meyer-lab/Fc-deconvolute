2382f88c4dd68dd4eeac8ff5f6ac168c7fbcf10e
Eva Hunter <75399938+evahunter@users.noreply.github.com>
2022-04-04 16:00:55 -0700

    scatterplots and colored fig1/3 (#40)
    * scatterplots and colored fig1/3
    
    * horizontal figs
    
    * add shape markers
    
    * fig4 and updated colors/shapes

--- deconv/figures/figure1.py
+++ deconv/figures/figure1.py
@@ -6,7 +6,15 @@ import numpy as np
 
 
 def makeFigure():
-    ax, f = getSetup((7, 10.5), (3, 2))
+    # set loadings marker color and shape
+    LoadingMarkers = ['o','o', '^', 'd', 's', 'v', 
+        'v', 'v', 'o', 'o', 'o', 'o']
+    LoadingColors =['lightcoral', 'lightcoral', 'gold', 'gold', 
+        'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
+        'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
+        'mediumturquoise', 'mediumturquoise']
+
+    ax, f = getSetup((14, 7), (2, 4))
 
     data_dekkers = load_dekkers()
     df = data_dekkers["profiling"]
@@ -19,10 +27,10 @@ def makeFigure():
     acc_variance = pca2.explained_variance_ratio_.copy()[:6]
     acc_variance = np.cumsum(acc_variance)
 
-    ax[0].plot(range(1, acc_variance.size + 1), acc_variance)
-    ax[0].set_ylabel("Explained Variance")
-    ax[0].set_xlabel("Number of Components")
-    ax[0].set_ylim([0, 1])
+    ax[1].plot(range(1, acc_variance.size + 1), acc_variance)
+    ax[1].set_ylabel("Explained Variance")
+    ax[1].set_xlabel("Number of Components")
+    ax[1].set_ylim([0, 1])
 
     ax[2].scatter(data_new[:, 0], data_new[:, 1])
     ax[2].set_title("Scores")
@@ -34,31 +42,30 @@ def makeFigure():
         ax[2].annotate(mixtures[i], (data_new[i, 0], data_new[i, 1]))
 
     loadings = pd.DataFrame(pca2.components_.T[:, :2], columns=['PC1', 'PC2'], index=data2.columns)
-    ax[3].scatter(loadings.iloc[:, 0], loadings.iloc[:, 1])
+    
     ax[3].set_title("Loadings")
     ax[3].set_xlabel("Component 1 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[0]))
     ax[3].set_ylabel("Component 2 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[1]))
 
     for i in range(12):
-        ax[3].annotate(data2.columns[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
+        ax[3].scatter(loadings.iloc[i, 0], loadings.iloc[i, 1], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
 
-    ax[4].scatter(data_new[:, 0], data_new[:, 2])
-    ax[4].set_title("Scores")
-    ax[4].set_xlabel("Component 1 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[0]))
-    ax[4].set_ylabel("Component 3 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[2]))
-
-    mixtures = data_dekkers["mixtures"]
-    for i in range(20):
-        ax[4].annotate(mixtures[i], (data_new[i, 0], data_new[i, 2]))
-
-    loadings = pd.DataFrame(pca2.components_.T[:, [0, 2]], columns=['PC1', 'PC3'], index=data2.columns)
-    ax[5].scatter(loadings.iloc[:, 0], loadings.iloc[:, 1])
-    ax[5].set_title("Loadings")
+    ax[5].scatter(data_new[:, 0], data_new[:, 2])
+    ax[5].set_title("Scores")
     ax[5].set_xlabel("Component 1 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[0]))
     ax[5].set_ylabel("Component 3 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[2]))
 
+    mixtures = data_dekkers["mixtures"]
+    for i in range(20):
+        ax[5].annotate(mixtures[i], (data_new[i, 0], data_new[i, 2]))
+
+    loadings = pd.DataFrame(pca2.components_.T[:, [0, 2]], columns=['PC1', 'PC3'], index=data2.columns)
+    ax[6].set_title("Loadings")
+    ax[6].set_xlabel("Component 1 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[0]))
+    ax[6].set_ylabel("Component 3 ({ratio:.0%})".format(ratio=pca2.explained_variance_ratio_[2]))
+
     for i in range(12):
-        ax[5].annotate(data2.columns[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
+        ax[6].scatter(loadings.iloc[i, 0], loadings.iloc[i, 1], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
 
     # Add subplot labels
     subplotLabel(ax)
--- deconv/figures/figure3.py
+++ deconv/figures/figure3.py
@@ -39,52 +39,63 @@ def makeFigure():
     lowErrL = np.subtract(median_loadings, ac33.T)
     highErrL = np.subtract(ac66.T, median_loadings)
 
+    # set up color and shape markers
+    ScoreMarkers = ['^','^','^', 'o','o', 'o', 'd','d', 
+        'D', 's', 's', 'X', 'x', 'x', 'x', '+', '+','+', '1', 
+        '1', '2', '3', '3', '4']
+    ScoreColor = ['orchid', 'darkblue', 'palegreen', 'orchid', 
+        'darkblue', 'palegreen', 'darkblue', 'palegreen', 'palegreen',
+         'darkblue', 'palegreen', 'palegreen','orchid', 'darkblue', 
+         'palegreen', 'orchid', 'darkblue', 'palegreen', 'darkblue', 
+         'palegreen' , 'palegreen','darkblue', 'palegreen' , 'palegreen']
+    LoadingMarkers = ['o','o', '^', 'd', 's', 'v', 'v', 'v', 'o', 'o', 
+        'o', 'o']
+    LoadingColors = ['lightcoral', 'lightcoral', 'gold', 'gold',
+         'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
+        'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
+        'mediumturquoise', 'mediumturquoise']
+
+
     ax, f = getSetup((7, 7), (2, 2))
 
     ax[0].set_title("Activity Scores")
     ax[0].set_xlabel("Component 1")
     ax[0].set_ylabel("Component 2")
-    #ax[0].set_ylim([0, 1.5])
-    #ax[0].set_xlim([0, 7])
-    ax[0].errorbar(median_scores[:, 0], median_scores[:, 1], yerr=[lowErrS[:, 1], highErrS[:, 1]], xerr=[lowErrS[:, 0], highErrS[:, 0]], fmt='o')
-    glycans = data_dekkers["glycans"]
-
-    scores = pd.DataFrame(median_scores)
+    ax[0].errorbar(median_scores[:, 0], median_scores[:, 1],
+         yerr=[lowErrS[:, 1], highErrS[:, 1]], xerr=[lowErrS[:, 0], 
+         highErrS[:, 0]],  fmt = ',', color ='k', lw = .5)
     for i in range(24):
-        ax[0].annotate(glycans[i], (scores.iloc[i, 0], scores.iloc[i, 1]))
-
+        ax[0].scatter(median_scores[i, 0], median_scores[i, 1], marker = ScoreMarkers[i], color = ScoreColor[i], edgecolor='k')
+    
     ax[1].set_title("Activity Loadings")
     ax[1].set_xlabel("Component 1")
     ax[1].set_ylabel("Component 2")
-    #ax[1].set_ylim([0, 1])
-    #ax[1].set_xlim([0, 1])
-    ax[1].errorbar(median_loadings[:, 0], median_loadings[:, 1], yerr=[lowErrL[:, 1], highErrL[:, 1]], xerr=[lowErrL[:, 0], highErrL[:, 0]], fmt='o')
-    labels = data2.columns
-    loadings = pd.DataFrame(median_loadings)
+    ax[1].errorbar(median_loadings[:, 0], median_loadings[:, 1],
+        yerr=[lowErrL[:, 1], highErrL[:, 1]], xerr=[lowErrL[:, 0], 
+        highErrL[:, 0]], fmt=',', color = 'k', lw = .5)
 
     for i in range(12):
-        ax[1].annotate(labels[i], (loadings.iloc[i, 0], loadings.iloc[i, 1]))
+        ax[1].scatter(median_loadings[i, 0], median_loadings[i, 1], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
 
     ax[2].set_title("Activity Scores")
     ax[2].set_xlabel("Component 1")
     ax[2].set_ylabel("Component 3")
-    #ax[2].set_ylim([0, 5])
-    #ax[2].set_xlim([0, 7])
-    ax[2].errorbar(median_scores[:, 0], median_scores[:, 2], yerr=[lowErrS[:, 2], highErrS[:, 2]], xerr=[lowErrS[:, 0], highErrS[:, 0]], fmt='o')
+    ax[2].errorbar(median_scores[:, 0], median_scores[:, 2], 
+        yerr=[lowErrS[:, 2], highErrS[:, 2]], xerr=[lowErrS[:, 0], 
+        highErrS[:, 0]], fmt=',', color = 'k', lw = .5)
 
-    scores = pd.DataFrame(median_scores)
     for i in range(24):
-        ax[2].annotate(glycans[i], (scores.iloc[i, 0], scores.iloc[i, 2]))
+        ax[2].scatter(median_scores[i, 0], median_scores[i, 2], marker = ScoreMarkers[i], color = ScoreColor[i], edgecolor='k')
 
     ax[3].set_title("Activity Loadings")
     ax[3].set_xlabel("Component 1")
     ax[3].set_ylabel("Component 3")
-    #ax[3].set_ylim([0, 0.3])
-    #ax[3].set_xlim([0, 1])
-    ax[3].errorbar(median_loadings[:, 0], median_loadings[:, 2], yerr=[lowErrL[:, 2], highErrL[:, 2]], xerr=[lowErrL[:, 0], highErrL[:, 0]], fmt='o')
+    ax[3].errorbar(median_loadings[:, 0], median_loadings[:, 2], 
+        yerr=[lowErrL[:, 2], highErrL[:, 2]], xerr=[lowErrL[:, 0], 
+        highErrL[:, 0]], fmt=',', color = 'k', lw = .5)
 
     for i in range(12):
-        ax[3].annotate(labels[i], (loadings.iloc[i, 0], loadings.iloc[i, 2]))
+        ax[3].scatter(median_loadings[i, 0], median_loadings[i, 2], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
 
     # Add subplot labels
     subplotLabel(ax)
