01987548c75d6db202e0e6576c5500cc9d9cb052
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2022-04-03 20:40:24 -0700

    Update figure2.py

--- deconv/figures/figure2.py
+++ deconv/figures/figure2.py
@@ -2,6 +2,7 @@ import numpy as np
 from .common import getSetup, subplotLabel
 from ..emceeDeconv import getEmceeTrace
 from ..imports import load_dekkers
+import matplotlib.ticker as mticker
 
 def makeFigure():
     ax, f = getSetup((7.5,7.5), (4, 3))
@@ -28,7 +29,10 @@ def makeFigure():
     for i in range(median.shape[1]):
         ax[i].errorbar(glycans, median[:, i], yerr=[p33[:, i], p66[:, i]], fmt='o')
         ax[i].set_ylabel(l[i], size=6)
+        ticks_loc = ax[i].get_xticks().tolist()
+        ax[i].xaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
         ax[i].set_xticklabels(glycans, rotation=90, size=6)
+
         _,_,_,y2 = ax[i].axis()
         ax[i].set_ylim([0, y2]) 
 
