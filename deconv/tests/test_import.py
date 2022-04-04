5e111865f43707e8deae9c67551ff8d3c2886492
Aaron Meyer <github@asmlab.org>
2022-04-04 10:42:46 -0700

    A start

---
+++ deconv/tests/test_import.py
@@ -0,0 +1,12 @@
+from ..imports import load_dekkers
+from ..emceeDeconv import getEmceeTrace
+
+
+def test_import():
+    """ Test that we can import the data. """
+    data = load_dekkers()
+
+
+def test_trace():
+    """ Fit the model. """
+    trace = getEmceeTrace()
