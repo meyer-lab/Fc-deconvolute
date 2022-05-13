094f248b0e8c31d7c628d515846a8b8d3f3c9b1d
Eva Hunter <ehunter@aretha.seas.ucla.edu>
2022-05-13 12:15:18 -0700

    fig3 and legend

--- deconv/figures/figure3.py
+++ deconv/figures/figure3.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from ..imports import load_dekkers
 from sklearn.decomposition import PCA
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
 
 
 def makeFigure():
@@ -42,18 +44,30 @@ def makeFigure():
     # set up color and shape markers
     ScoreMarkers = ['^','^','^', 'o','o', 'o', 'd','d', 
         'D', 's', 's', 'X', 'x', 'x', 'x', '+', '+','+', '1', 
-        '1', '2', '3', '3', '4']
-    ScoreColor = ['orchid', 'darkblue', 'palegreen', 'orchid', 
-        'darkblue', 'palegreen', 'darkblue', 'palegreen', 'palegreen',
-         'darkblue', 'palegreen', 'palegreen','orchid', 'darkblue', 
-         'palegreen', 'orchid', 'darkblue', 'palegreen', 'darkblue', 
-         'palegreen' , 'palegreen','darkblue', 'palegreen' , 'palegreen']
+        '1', '2', '|', '|', '_']
+    ScoreColor = ['crimson', 'orchid', 'blue', 'crimson', 
+         'orchid', 'blue', 'orchid', 'blue', 'blue',
+         'orchid', 'blue', 'blue','crimson', 'orchid', 
+         'blue', 'crimson', 'orchid', 'blue', 'orchid', 
+         'blue' , 'blue','orchid', 'blue' , 'blue']
     LoadingMarkers = ['o','o', 'o', 'o', 'o', 'o', 'v', 
         'v', 'v', 's', '^', 'd']
     LoadingColors =['lightcoral', 'lightcoral',  
         'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
         'mediumturquoise', 'mediumturquoise', 'mediumturquoise', 
         'mediumturquoise', 'mediumturquoise','gold', 'gold']
+
+    #set up legend elements
+    legend_elements1 = [mlines.Line2D([], [], marker='^', markeredgecolor='k', label='Complement C1q',markersize=12, ls = 'none', markerfacecolor = 'none'),
+                   mlines.Line2D([], [], marker='d', markeredgecolor='k', label='Complement C4',markersize=12, ls = 'none', markerfacecolor = 'none'),
+                   mlines.Line2D([], [], marker='s', markeredgecolor='k', label='FcγRIa',markersize=12, ls = 'none', markerfacecolor = 'none'),
+                   mlines.Line2D([], [], marker='v', markeredgecolor='k', label='FcγRII',markersize=12, ls = 'none', markerfacecolor = 'none'),
+                   mlines.Line2D([], [], marker='o', markeredgecolor='k', label='FcγRIII',markersize=12, ls = 'none', markerfacecolor=  'none')]
+
+
+    legend_elements2 = [mpatches.Patch(facecolor = 'lightcoral', label = 'ADCC'),
+                   mpatches.Patch(facecolor = 'gold', label = 'Complement Activation'),
+                   mpatches.Patch(facecolor = 'mediumturquoise', label = 'Binding')]
 
 
     ax, f = getSetup((7, 7), (2, 2))
@@ -63,16 +77,25 @@ def makeFigure():
     ax[0].set_ylabel("Component 2")
     ax[0].errorbar(median_scores[:, 0], median_scores[:, 1],
          yerr=[lowErrS[:, 1], highErrS[:, 1]], xerr=[lowErrS[:, 0], 
-         highErrS[:, 0]],  fmt = ',', color ='k', lw = .5)
+         highErrS[:, 0]],  fmt = ',', color ='k', lw = .25)
     for i in range(24):
         ax[0].scatter(median_scores[i, 0], median_scores[i, 1], marker = ScoreMarkers[i], color = ScoreColor[i], edgecolor='k')
-    
+
+    legend1 = ax[0].legend(handles= legend_elements1, bbox_to_anchor=(0., 1.25, 1., .25), loc='lower left',
+                      ncol=1, borderaxespad=0., prop= {'size': 11}, handlelength = 2)
+
+    ax[0].add_artist(legend1)
+
     ax[1].set_title("Activity Loadings")
     ax[1].set_xlabel("Component 1")
     ax[1].set_ylabel("Component 2")
     ax[1].errorbar(median_loadings[:, 0], median_loadings[:, 1],
         yerr=[lowErrL[:, 1], highErrL[:, 1]], xerr=[lowErrL[:, 0], 
-        highErrL[:, 0]], fmt=',', color = 'k', lw = .5)
+        highErrL[:, 0]], fmt=',', color = 'k', lw = .25)
+
+    legend2 = ax[1].legend(handles= legend_elements2, bbox_to_anchor=(0., 1.25, .7, .25), loc='lower right',
+                      ncol=1, borderaxespad=0., prop= {'size': 12}, handleheight = 2)
+    ax[1].add_artist(legend2)
 
     for i in range(12):
         ax[1].scatter(median_loadings[i, 0], median_loadings[i, 1], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
@@ -82,7 +105,7 @@ def makeFigure():
     ax[2].set_ylabel("Component 3")
     ax[2].errorbar(median_scores[:, 0], median_scores[:, 2], 
         yerr=[lowErrS[:, 2], highErrS[:, 2]], xerr=[lowErrS[:, 0], 
-        highErrS[:, 0]], fmt=',', color = 'k', lw = .5)
+        highErrS[:, 0]], fmt=',', color = 'k', lw = .25)
 
     for i in range(24):
         ax[2].scatter(median_scores[i, 0], median_scores[i, 2], marker = ScoreMarkers[i], color = ScoreColor[i], edgecolor='k')
@@ -92,7 +115,7 @@ def makeFigure():
     ax[3].set_ylabel("Component 3")
     ax[3].errorbar(median_loadings[:, 0], median_loadings[:, 2], 
         yerr=[lowErrL[:, 2], highErrL[:, 2]], xerr=[lowErrL[:, 0], 
-        highErrL[:, 0]], fmt=',', color = 'k', lw = .5)
+        highErrL[:, 0]], fmt=',', color = 'k', lw = .25)
 
     for i in range(12):
         ax[3].scatter(median_loadings[i, 0], median_loadings[i, 2], marker = LoadingMarkers[i], color = LoadingColors[i], edgecolor='k')
