351d46764279aa31e1ca9b58a867ddcf6e32f008
Aaron Meyer <ameyer@ucla.edu>
2022-05-16 11:33:38 -0700

    Normalize mixtures and activities

--- deconv/emceeDeconv.py
+++ deconv/emceeDeconv.py
@@ -1,24 +1,28 @@
 
 from .imports import load_dekkers
+import numpy as np
 import pymc as pm
-from aesara.tensor import dot
+import aesara.tensor as T
 
 
 def getEmceeTrace():
     data_dekkers = load_dekkers()
 
     A_antiD = data_dekkers["antiD"]
+    A_antiD /= np.sum(A_antiD, axis=1, keepdims=True)
 
     df = data_dekkers["profiling"]
     data = df.groupby(["index", "receptor"]).mean().reset_index()
     res = data.pivot(index="receptor", columns="index", values="binding").values
+    res /= np.mean(res, axis=1, keepdims=True)
+    res = res.T
 
     M = pm.Model()
 
     with M:
-        activity = pm.Lognormal("activity", sigma=1.0, shape=(24, 12))
-        predict = dot(A_antiD, activity).T
-        pm.Lognormal("fit", mu=predict, sigma=0.2, observed=res)
+        activity = pm.Lognormal("activity", sigma=1.0, mu=-1.0, shape=(24, 12))
+        predict = T.dot(A_antiD, activity)
+        pm.Lognormal("fit", mu=predict, sigma=0.1, observed=res)
 
-    trace = pm.sample(500, tune=5000, model=M, return_inferencedata=True, target_accept=0.95)
+    trace = pm.sample(3000, tune=3000, model=M, return_inferencedata=True, target_accept=0.95)
     return trace
